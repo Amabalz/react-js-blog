08bac0fb4b8ab6dfab830bff00ad3aab38dd91f9
Petcut Adrian <iq2adi@gmail.com>
2024-02-20 13:00:44 +0200

    all

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Header from './Header'
+import Nav from './Nav'
+import Footer from './Footer'
+import Home from './Home'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import About from './About'
+import Missing from './Missing'
+import EditPost from './EditPost'
+import { Route, Routes } from 'react-router-dom'
+import { DataProvider } from './context/DataContext'
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="React JS Blog"/>
+      <DataProvider>
+        <Nav/>
+        <Routes>
+          <Route path="/" Component={Home}></Route>
+          <Route path="/post" Component={NewPost}></Route>
+          <Route path="/edit/:id" Component={EditPost}></Route>
+          <Route path="/post/:id" Component={PostPage}></Route>
+          <Route path="/about" Component={About}></Route>
+          <Route path="*" Component={Missing}></Route>
+        </Routes>
+      </DataProvider>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
